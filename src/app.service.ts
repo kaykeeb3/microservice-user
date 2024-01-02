1e3dca2eaa1ec4a631eb4e2ba6105fcbae4a7a43
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-02 10:02:52 -0300

    feat: creating the first (Route), deleting useless files & preparing the environment

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  create({ name, email }: { name: string; email: string }): string {
+    return `Olá, ${name}`;
   }
 }
