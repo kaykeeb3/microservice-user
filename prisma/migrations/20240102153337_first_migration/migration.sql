80eb1132972c75fb28edf628b2aa21bd44b1e08e
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-02 12:36:33 -0300

    feat: add migration of database tables (PRISMA)

---
+++ prisma/migrations/20240102153337_first_migration/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
