31d441fe6314efc15e80ac5b6a6bc5e023a0e326
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-02 09:45:43 -0300

    refactor: configuring (.eslintrc.js) for windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,18 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
+  "ignorePatterns": ["**/*.js", '*.js'],
+  'rules': {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': ["error", { 'allow': ["warn", "error"]}],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
